57e21c941a56bed24663f1112a5039cafeb3a3da
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2025-06-25 11:17:05 +0100

    chore: Create new repo for celery

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Any
 
 from omegaconf import DictConfig, OmegaConf
 from pydantic import Field
@@ -69,6 +70,38 @@ class BatchData(BaseSchema):
     download_path: str = Field(description="Download path")
     batch_mode: bool = Field(description="Whether to use batch mode")
     batch_size: int = Field(description="Batch size")
+
+
+class Celery(BaseSchema):
+    """Celery configuration class."""
+
+    broker_url: str = Field(description="Broker URL")
+    result_backend: str = Field(description="Result backend")
+
+    task_configuration: TaskConfiguration = Field(description="Task configuration")
+    task_routes: dict[str, Any] = Field(description="Task routes")
+    worker_configuration: WorkerConfiguration = Field(description="Worker configuration")
+    beat_configuration: BeatConfiguration = Field(description="Beat configuration")
+
+
+class TaskConfiguration(BaseSchema):
+    """Task configuration class."""
+
+    task_serializer: str
+    result_serializer: str
+    timezone: str
+    enable_utc: str
+
+
+class WorkerConfiguration(BaseSchema):
+    worker_prefetch_multiplier: int
+    task_acks_late: bool
+    worker_max_tasks_per_child: int
+
+
+class BeatConfiguration(BaseSchema):
+    beat_schedule: dict[str, Any]
+    health_check: dict[str, Any]
 
 
 class AppConfig(BaseSchema):
